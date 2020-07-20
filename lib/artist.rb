17c83250749320660dccb53e4e59e53484f8e692
Michael Waide <mvwaide@gmail.com>
2020-07-20 17:42:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,31 @@
-class Artist
-  attr_accessor :name
+class Artist	
+  attr_accessor :name	
 
-  @@song_count = 0
+  @@all = []	
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+  def initialize(name)	
+    @name = name	
+    @@all << self	
+  end	
 
-  def add_song(song)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
+  def self.all	
+    @@all	
+  end	
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
+  def add_song(song)	
+    song.artist = self	
+  end	
 
-  def songs
-    @songs
-  end
+  def add_song_by_name(name)	
+    song = Song.new(name)	
+    song.artist = self	
+  end	
 
-  def self.song_count
-    @@song_count
-  end
+  def songs	
+    Song.all.select {|song| song.artist == self}	
+  end	
+
+  def self.song_count	
+    Song.all.count	
+  end	
 end
