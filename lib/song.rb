68a93a62542c20dffe8919fbf3c3e73e626fc104
Michael Waide <mvwaide@gmail.com>
2020-07-20 17:43:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,36 @@
-class Song	
-  attr_accessor :name, :artist	
+class Song
+  attr_accessor :name, :artist
 
-  @@all = []	
+  @@all = []
 
-  def initialize(name)	
-    @name = name	
-    @@all << self	
-  end	
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-  def self.all	
-    @@all	
-  end	
+  def self.all
+    @@all
+  end
 
-  def artist_name	
-    artist.name if artist	
-  end	
+  def artist_name
+    artist.name if artist
+  end
 end
-class Song	
-  attr_accessor :name, :artist	
+class Song
+  attr_accessor :name, :artist
 
-  @@all = []	
+  @@all = []
 
-  def initialize(name)	
-    @name = name	
-    @@all << self	
-  end	
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-  def self.all	
-    @@all	
-  end	
+  def self.all
+    @@all
+  end
 
-  def artist_name	
-    artist.name if artist	
-  end	
+  def artist_name
+    artist.name if artist
+  end
 end
